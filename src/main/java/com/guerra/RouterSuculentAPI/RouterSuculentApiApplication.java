7c07f6173246978c60dae986e8e8eb670d85a8a6
jaasiel17 <jaasielguerra17@gmail.com>
2023-07-24 20:50:29 -0600

    Proyecto base

---
+++ src/main/java/com/guerra/RouterSuculentAPI/RouterSuculentApiApplication.java
@@ -0,0 +1,13 @@
+package com.guerra.RouterSuculentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterSuculentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterSuculentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guerra/RouterSuculentAPI/RouterSuculentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerra.RouterSuculentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterSuculentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
